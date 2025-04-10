af05500313df7ad52f9683eaf6ea3388327bb39a
Maximilian Rau <st188391@stud.uni-stuttgart.de>
2025-04-10 20:39:59 +0200

    init backend

---
+++ backend/src/main/java/com/cinemate/CineMateApplication.java
@@ -0,0 +1,13 @@
+package com.cinemate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CineMateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CineMateApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/cinemate/CineMateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cinemate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CineMateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
